abc9922b71fe2f3282640e7af803c4683df64ed6
gomberg5264 <61816796+gomberg5264@users.noreply.github.com>
2021-01-09 20:24:10 -0500

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+console.log("Hello how are you today!");
+console.clear();
+if (4 > 2) {
+  console.log("It was greater");
+} else {
+  console.log("it was not greater");
+}
+function sum(x, y) {
+  let answer = x + y;
+  return answer;
+}
+sum(7, 73);
+
+let result = sum(7, 73);
+let result2 = sum(55, 55);
+console.log(result2);
